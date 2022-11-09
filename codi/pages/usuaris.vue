9d9c2df94a07a3a66aef0c0b72d719150531c0eb
mand777 <amandvieira7@gmail.com>
2022-11-09 15:38:39 +0100

    commi

---
+++ codi/pages/usuaris.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+
+    </div>
+</template>
+<script>
+
+fetch("https://dummyjson.com/users")
+.then(res=>{
+    return res.json()
+})
+.then((usuaris)=>{
+    console.log(usuaris)
+});
+
+console.log("despres del fetch")
+
+fetch("https://dummyjson.com/users/1")
+.then(res.json())
+.then(console.log);
+
+</script>
